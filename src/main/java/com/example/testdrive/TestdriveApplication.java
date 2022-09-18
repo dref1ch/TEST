9ce112e270710fbcebc2ce479948178658b3fe96
dref1ch <chrom2626@gmail.com>
2022-09-18 20:07:47 +0300

    New commit

---
+++ src/main/java/com/example/testdrive/TestdriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestdriveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testdrive/TestdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
