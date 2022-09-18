6341375b839734bd759f3b19f198008ad3ff83bd
dref1ch <chrom2626@gmail.com>
2022-09-18 20:38:04 +0300

    frankivsk Commit

---
+++ src/main/java/com/example/testdrive/Frankivsk.java
@@ -0,0 +1,10 @@
+package com.example.testdrive;/*
+    @author Denys
+    @project TESTDRIVE
+    @class Frankivsk
+    @version 1.0.0
+    @since 18.09.2022 - 20 - 37
+*/
+
+public class Frankivsk {
+}
